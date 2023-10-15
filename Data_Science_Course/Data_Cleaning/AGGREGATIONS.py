bf10abb9bd9a098bec0fae80ae9376bfbf1ab049
Kumar <uif30109@contiwan.com>
2023-10-15 12:39:49 +0530

    ychanges

--- Data_Science_Course/Data_Cleaning/AGGREGATIONS.py
+++ Data_Science_Course/Data_Cleaning/AGGREGATIONS.py
@@ -4,22 +4,35 @@
 
 import numpy as np
 import pandas as pd
-data = {
-    "company": ['GOOG', 'GOOG', 'MSFT', 'MSFT', 'FB', 'FB'],
-    "person": ['sam', 'charlie', 'amy', 'vanessa', 'carl', 'sarah'],
-    "sale": [200, 120, 340, 124, 243, 350]
-}
-list =['a', 'b', 'c', 'd', 'e', 'f']
-df = pd.DataFrame(data, index= list)
+# data = {
+#     "company": ['GOOG', 'GOOG', 'MSFT', 'MSFT', 'FB', 'FB'],
+#     "person": ['sam', 'charlie', 'amy', 'vanessa', 'carl', 'sarah'],
+#     "sale": [200, 120, 340, 124, 243, 350]
+# }
+# list =['a', 'b', 'c', 'd', 'e', 'f']
+# df = pd.DataFrame(data, index= list)
 # print(df)
-df_new = df.groupby('company')   #### with the groupby we have use something for aggregation function
-# print(df_new.mean())  ### her we are aggregating with the mean function
+# df_new = df.groupby('company')   #### with the groupby we have use something for aggregation function
+# # print(df_new.mean())  ### her we are aggregating with the mean function
+#
+# # print(df.groupby('company').sum().loc['FB'])  ### this is one line code for a group by and sum function on the sale
+#                                               ### and then returning the value for the index FB
+# """   using describe function   """
+# print(df.groupby('company').describe()) ### this will return the information for the index company and the sales
+# print(df.groupby('company').describe().transpose())   ### this will return the transposed matrix with company as the columns
 
-# print(df.groupby('company').sum().loc['FB'])  ### this is one line code for a group by and sum function on the sale
-                                              ### and then returning the value for the index FB
-"""   using describe function   """
-print(df.groupby('company').describe()) ### this will return the information for the index company and the sales
-print(df.groupby('company').describe().transpose())   ### this will return the transposed matrix with company as the columns
 
 
 
+
+
+"""" A way of defining a data frame through dictionary , when the dictionary is not defined """
+# train = pd.DataFrame(dict(Id=np.arange(1437), A=np.arange(1437)))
+# print(train.shape)
+# print(train.Id.shape)
+#
+# (1437, 2)
+# (1437,)
+
+dt = pd.DataFrame(dict(Id=np.arange(1437), A=np.arange(1437)), index= np.arange(1437))
+print(dt)
